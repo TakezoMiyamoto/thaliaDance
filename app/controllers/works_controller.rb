5a42d7979e583b34bc738fa7d718e7517c9ff84b
takezo <takezo.m.1987@gmail.com>
2018-10-02 23:24:26 +0900

    #1 search and show view youtube movies

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,14 +1,50 @@
 class WorksController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
-  
-  def index
-    @works = Work.order(created_at: :desc)
-    @title = 'ワークス一覧'
-  end
 
-  def show
-    @work = Work.find(params[:id])
-  end
+    def index
+      get_data("formeraction")
+
+    end
+
+    def test
+      return a=1
+    end
+
+    def get_data(keyword)
+      require 'youtube.rb'#先ほど上で準備したファイルを呼ぶ
+      opts = Trollop::options do
+        opt :q, 'Search term', :type => String, :default => keyword
+        opt :max_results, 'Max results', :type => :int, :default => 25
+        opt :order, 'order', :type => String, :default => 'date'
+        opt :regionCode, 'region', :type => String, :default => 'JP'
+      end
+
+      client, youtube = get_service
+
+      begin
+
+        search_response = client.execute!(
+          :api_method => youtube.search.list,
+          :parameters => {
+            :part => 'snippet',
+            :q => opts[:q],
+            :maxResults => opts[:max_results],
+            :order => opts[:order],
+            :regionCode => opts[:regionCode]
+          }
+        )
+
+        @works = []
+          search_response.data.items.each do |search_result|
+           @works << search_result.id.video_id
+          end
+
+      rescue Google::APIClient::TransmissionError => e
+        puts e.result.body
+      end
+    end
+
+
 
   private
 
