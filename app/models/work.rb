85c41534a50331bfa952f7c6866805526a378505
takezo <takezo.m.1987@gmail.com>
2018-10-02 22:40:42 +0900

    #1 modify work model for youtube api

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,28 @@
 class Work < ApplicationRecord
+  belongs_to :user
+
+  require 'rubygems'
+  require 'google/api_client'
+  require 'trollop'
+
+  DEVELOPER_KEY = ENV['DEVELOPER_KEY']
+  YOUTUBE_API_SERVICE_NAME = 'youtube'
+  YOUTUBE_API_VERSION = 'v3'
+
+  def get_service
+    client = Google::APIClient.new(
+      :key => DEVELOPER_KEY,
+      :authorization => nil,
+      :application_name => $PROGRAM_NAME,
+      :application_version => '1.0.0'
+    )
+    youtube = client.discovered_api(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION)
+
+    return client, youtube
+  end
+
+
+
+
+
 end
