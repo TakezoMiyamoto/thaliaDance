751e120bbe129865320cc00b92bbf7f9723a07a5
takezo <takezo.m.1987@gmail.com>
2018-10-04 01:37:44 +0900

    #2 retrieve my youtube upload

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,51 +2,106 @@ class WorksController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
     def index
-      get_data("formeraction")
-
+      # get_data("formeraction")
+      main
     end
 
     def test
       return a=1
     end
 
-    def get_data(keyword)
-      require 'youtube.rb'#先ほど上で準備したファイルを呼ぶ
-      opts = Trollop::options do
-        opt :q, 'Search term', :type => String, :default => keyword
-        opt :max_results, 'Max results', :type => :int, :default => 25
-        opt :order, 'order', :type => String, :default => 'date'
-        opt :regionCode, 'region', :type => String, :default => 'JP'
-      end
-
-      client, youtube = get_service
-
-      begin
-
-        search_response = client.execute!(
-          :api_method => youtube.search.list,
-          :parameters => {
-            :part => 'snippet',
-            :q => opts[:q],
-            :maxResults => opts[:max_results],
-            :order => opts[:order],
-            :regionCode => opts[:regionCode]
-          }
-        )
-
-        @works = []
-          search_response.data.items.each do |search_result|
-           @works << search_result.id.video_id
-          end
-
-      rescue Google::APIClient::TransmissionError => e
-        puts e.result.body
-      end
-    end
 
 
 
   private
+  def main
+    require 'youtube.rb'
+    client, youtube = get_authenticated_service
+
+      begin
+        # Retrieve the "contentDetails" part of the channel resource for the
+        # authenticated user's channel.
+        channels_response = client.execute!(
+          :api_method => youtube.channels.list,
+          :parameters => {
+            :mine => true,
+            :part => 'contentDetails'
+          }
+        )
+
+        channels_response.data.items.each do |channel|
+          # From the API response, extract the playlist ID that identifies the list
+          # of videos uploaded to the authenticated user's channel.
+          uploads_list_id = channel['contentDetails']['relatedPlaylists']['uploads']
+
+          # Retrieve the list of videos uploaded to the authenticated user's channel.
+          next_page_token = ''
+          until next_page_token.nil?
+            playlistitems_response = client.execute!(
+              :api_method => youtube.playlist_items.list,
+              :parameters => {
+                :playlistId => uploads_list_id,
+                :part => 'snippet',
+                :maxResults => 25,
+                :pageToken => next_page_token
+              }
+            )
+
+            @works = []
+
+             # Print information about each video.
+             playlistitems_response.data.items.each do |playlist_item|
+               @works << playlist_item.snippet.resourceId.videoId
+
+             end
+
+             next_page_token = playlistitems_response.next_page_token
+           end
+
+           puts
+        end
+      rescue Google::APIClient::TransmissionError => e
+        puts e.result.body
+      end
+    end
+  end
+
+
+# -------------------------------------------------------------------------
+  def get_data(keyword)
+    require 'youtube.rb'#先ほど上で準備したファイルを呼ぶ
+    opts = Trollop::options do
+      opt :q, 'Search term', :type => String, :default => keyword
+      opt :max_results, 'Max results', :type => :int, :default => 25
+      opt :order, 'order', :type => String, :default => 'date'
+      opt :regionCode, 'region', :type => String, :default => 'JP'
+    end
+
+    client, youtube = get_service
+
+    begin
+
+      search_response = client.execute!(
+        :api_method => youtube.search.list,
+        :parameters => {
+          :part => 'snippet',
+          :q => opts[:q],
+          :maxResults => opts[:max_results],
+          :order => opts[:order],
+          :regionCode => opts[:regionCode]
+        }
+      )
+
+      @works = []
+        search_response.data.items.each do |search_result|
+         @works << search_result.id.video_id
+        end
+
+    rescue Google::APIClient::TransmissionError => e
+      puts e.result.body
+    end
+  end
+# -------------------------------------------------------------------------
 
   def works_params
     params.require(:work).permit(:title, :youtube_id, :thumbnail, :description, :youtube_url)
@@ -63,4 +118,3 @@ class WorksController < ApplicationController
       redirect_to works_path
     end
   end
-end
--- app/models/youtube.rb
+++ app/models/youtube.rb
@@ -1,19 +1,129 @@
 require 'rubygems'
 require 'google/api_client'
+require 'google/api_client/client_secrets'
+require 'google/api_client/auth/file_storage'
+require 'google/api_client/auth/installed_app'
 require 'trollop'
+require 'json'
+require 'launchy'
+
+# RESPONSE_HTML = <<stop
+#
+# stop
+#
+# FILE_POSTFIX = '-oauth2.json'
+#
+# # Small helper for the sample apps for performing OAuth 2.0 flows from the command
+# # line. Starts an embedded server to handle redirects.
+# class CommandLineOAuthHelper
+#
+#   def initialize(scope)
+#     credentials = Google::APIClient::ClientSecrets.load
+#     @authorization = Signet::OAuth2::Client.new(
+#       :authorization_uri => credentials.authorization_uri,
+#       :token_credential_uri => credentials.token_credential_uri,
+#       :client_id => credentials.client_id,
+#       :client_secret => credentials.client_secret,
+#       :redirect_uri => credentials.redirect_uris.first,
+#       :scope => scope
+#     )
+#   end
+#
+#   # Request authorization. Checks to see if a local file with credentials is present, and uses that.
+#   # Otherwise, opens a browser and waits for response, then saves the credentials locally.
+#   def authorize
+#     credentialsFile = $0 + FILE_POSTFIX
+#
+#     if File.exist? credentialsFile
+#       File.open(credentialsFile, 'r') do |file|
+#         credentials = JSON.load(file)
+#         @authorization.access_token = credentials['access_token']
+#         @authorization.client_id = credentials['client_id']
+#         @authorization.client_secret = credentials['client_secret']
+#         @authorization.refresh_token = credentials['refresh_token']
+#         @authorization.expires_in = (Time.parse(credentials['token_expiry']) - Time.now).ceil
+#         if @authorization.expired?
+#           @authorization.fetch_access_token!
+#           save(credentialsFile)
+#         end
+#       end
+#     else
+#       auth = @authorization
+#       url = @authorization.authorization_uri().to_s
+#       server = Thin::Server.new('0.0.0.0', 8080) do
+#         run lambda { |env|
+#           # Exchange the auth code & quit
+#           req = Rack::Request.new(env)
+#           auth.code = req['code']
+#           auth.fetch_access_token!
+#           server.stop()
+#           [200, {'Content-Type' => 'text/html'}, RESPONSE_HTML]
+#         }
+#       end
+#
+#       Launchy.open(url)
+#       server.start()
+#
+#       save(credentialsFile)
+#     end
+#
+#     return @authorization
+#   end
+#
+#   def save(credentialsFile)
+#     File.open(credentialsFile, 'w', 0600) do |file|
+#       json = JSON.dump({
+#         :access_token => @authorization.access_token,
+#         :client_id => @authorization.client_id,
+#         :client_secret => @authorization.client_secret,
+#         :refresh_token => @authorization.refresh_token,
+#         :token_expiry => @authorization.expires_at
+#       })
+#       file.write(json)
+#     end
+#   end
+# end
+#
+
 
 DEVELOPER_KEY = ENV['DEVELOPER_KEY']
+# This OAuth 2.0 access scope allows for read-only access to the authenticated
+# user's account, but not other types of account access.
+YOUTUBE_READONLY_SCOPE = 'https://www.googleapis.com/auth/youtube.readonly'
 YOUTUBE_API_SERVICE_NAME = 'youtube'
 YOUTUBE_API_VERSION = 'v3'
 
-def get_service
+# def get_service
+#   client = Google::APIClient.new(
+#     :key => DEVELOPER_KEY,
+#     :authorization => nil,
+#     :application_name => $PROGRAM_NAME,
+#     :application_version => '1.0.0'
+#   )
+#   youtube = client.discovered_api(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION)
+#
+#   return client, youtube
+# end
+
+def get_authenticated_service
   client = Google::APIClient.new(
-    :key => DEVELOPER_KEY,
-    :authorization => nil,
     :application_name => $PROGRAM_NAME,
     :application_version => '1.0.0'
   )
   youtube = client.discovered_api(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION)
+
+  file_storage = Google::APIClient::FileStorage.new("#{$PROGRAM_NAME}-oauth2.json")
+  if file_storage.authorization.nil?
+    client_secrets = Google::APIClient::ClientSecrets.load
+    flow = Google::APIClient::InstalledAppFlow.new(
+      :client_id => client_secrets.client_id,
+      :client_secret => client_secrets.client_secret,
+      :scope => [YOUTUBE_READONLY_SCOPE]
+    )
+    client.authorization = flow.authorize(file_storage)
+  else
+    client.authorization = file_storage.authorization
+  end
 
   return client, youtube
 end
